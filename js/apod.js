9fa969756389bf3c7fdcbca5df819eb3ce60dba8
Holly MacLean <mariettia@gmail.com>
2024-12-02 16:16:13 -0500

    added function to display APOD on the page

--- js/apod.js
+++ js/apod.js
@@ -1,3 +1,9 @@
+//Closely following lesson 11 nytimes lesson example for structure
+//https://api.nasa.gov/ - for nasa api getting started and available apis
+//https://apod.nasa.gov/apod/astropix.html - APOD website
+//https://github.com/nasa/apod-api - provides extra information on the api, including available fields
+
+
 const baseUrl = 'https://api.nasa.gov/planetary/apod';
 const key = 'OnIbR9o5oEN0Pq90P3lqRunpOdJhijj5OIzmzLRh';
 let url;
@@ -5,7 +11,7 @@ let url;
 //Reference to DOM elements
 const dateInput = document.querySelector('.date');
 const title = document.querySelector('.title');
-const date = document.querySelector('.date');
+const date = document.querySelector('.imageDate');
 const apodImg = document.querySelector('.apod');
 
 //Function to fetch APOD
@@ -16,12 +22,20 @@ function fetchApod(date = '') { // date may or many not be provided
         url += `&date=${date}`;
     }
 
-    //fetching using the combined url
+    //fetching data using the combined url
     fetch(url)
         .then(result =>{ return result.json()})
         .then(json =>{displayAPOD(json)})
         .catch(error=>{
             console.log('Error getting APOD:',error);
         });
+}
+
+//function displays the APOD
+function displayAPOD(json) {
+    title.textContent = json.title;
+    date.textContent = json.date;
+    apodImg.src = json.url;
+    apodImg.alt = json.title;
 }
 
