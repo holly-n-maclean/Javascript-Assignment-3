f52fe45a0a2c008700eb7a623bae520e6ba555a9
Holly MacLean <mariettia@gmail.com>
2024-12-02 15:51:26 -0500

    Added function to fetch data

--- js/apod.js
+++ js/apod.js
@@ -8,3 +8,20 @@ const title = document.querySelector('.title');
 const date = document.querySelector('.date');
 const apodImg = document.querySelector('.apod');
 
+//Function to fetch APOD
+function fetchApod(date = '') { // date may or many not be provided
+    url = `${baseUrl}?api_key=${key}`; //base url + api
+    //if statement to add date if provided to the url
+    if(date) {
+        url += `&date=${date}`;
+    }
+
+    //fetching using the combined url
+    fetch(url)
+        .then(result =>{ return result.json()})
+        .then(json =>{displayAPOD(json)})
+        .catch(error=>{
+            console.log('Error getting APOD:',error);
+        });
+}
+
