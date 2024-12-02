1487068e83fdda0a951630c93ec2fd72501efbf3
Holly MacLean <mariettia@gmail.com>
2024-12-02 15:37:28 -0500

    Added DOM elements

--- js/apod.js
+++ js/apod.js
@@ -2,3 +2,9 @@ const baseUrl = 'https://api.nasa.gov/planetary/apod';
 const key = 'OnIbR9o5oEN0Pq90P3lqRunpOdJhijj5OIzmzLRh';
 let url;
 
+//Reference to DOM elements
+const dateInput = document.querySelector('.date');
+const title = document.querySelector('.title');
+const date = document.querySelector('.date');
+const apodImg = document.querySelector('.apod');
+
